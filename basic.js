bfe011501769e01d58bb09176c3d943cd28eed56
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-12 04:19:06 +0000

    Jul 12, 2026, 4:19 AM

--- basic.js
+++ basic.js
@@ -78,7 +78,7 @@ function setup() {
 // --- Exercise: Age category classification ---
   // write your codes here
   
-  let age = Math.random(1, 1000);
+  let age = Math.random(6, 1000);
   let category = "";
   if (age <= 9){
     category = "lower primary";

203f9ae144b9699703eaf51ff6ab69ca3d456f12
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-12 04:14:30 +0000

    Jul 12, 2026, 4:14 AM

--- basic.js
+++ basic.js
@@ -75,22 +75,6 @@ function setup() {
   };
   text("You are " + age + " years old in " +  category, 20, 120);
 
-// --- Exercise: Display odd numbers backward using while loop ---
-  // write your codes here
-
-  let oddSum = 0;
-  let num = 19;
-  let xOdd = 20;
-  let yOdd = yEven + 100;
-  while (num >= 1){
-    console.log(num);
-    xOdd += 50;
-    oddSum = oddSum + num;
-    num = num - 2;
-  }
-  console.log(oddSum);
-  text("The sum of the first 10 odd numbers is " + oddSum, 20, 90);
-
 // --- Exercise: Array operations (groceries) ---
   // write your codes here
 

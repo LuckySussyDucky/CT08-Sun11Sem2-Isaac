4d7db12ec0f96917cd800a0ac7b31bb4612d420c
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-12 04:06:02 +0000

    Jul 12, 2026, 4:06 AM

--- basic.js
+++ basic.js
@@ -34,6 +34,8 @@ function setup() {
     console.log(i);
   }
 
+// --- Exercise: Sum of first 10 even numbers ---
+  // write your codes here\
   for(let i = 0; i < 46; i++){
     if(i % 3 == 0){
       console.log(i);

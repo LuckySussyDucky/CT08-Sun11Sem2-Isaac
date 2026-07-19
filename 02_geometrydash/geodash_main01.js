30023acdea6e3ad1a8f717605a3e6900082d0e74
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-19 04:31:39 +0000

    Jul 19, 2026, 4:31 AM

--- 02_geometrydash/geodash_main01.js
+++ 02_geometrydash/geodash_main01.js
@@ -84,9 +84,9 @@ function setup() {
   new Tiles(tileMap1, 0, 0, 50, 50);
 }
 
-function draw() {
-  clear();
-  image(background, 0, 0, 800, 600);
-}
+// function draw() {
+//   clear();
+//   image(background, 0, 0, 800, 600);
+// }
 
 

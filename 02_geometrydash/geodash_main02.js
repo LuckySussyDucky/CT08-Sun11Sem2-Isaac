2162dde976bb1e0a4bd68e6aeb66e16026f286a3
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-26 04:03:35 +0000

    Jul 26, 2026, 4:03 AM

--- 02_geometrydash/geodash_main02.js
+++ 02_geometrydash/geodash_main02.js
@@ -126,7 +126,8 @@ function draw() {
       let leftEdgeHeight = tile.y - tile.h / 2;
 
       if (box.x < leftEdge && box.y > leftEdgeHeight){
-        resetGame
+        resetGame();
+        break;
       }
     }
   }

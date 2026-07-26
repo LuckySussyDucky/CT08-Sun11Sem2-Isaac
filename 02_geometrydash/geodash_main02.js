c38065187baab6b3969c09ecd19569d8b450423e
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-26 04:24:05 +0000

    Jul 26, 2026, 4:24 AM

--- 02_geometrydash/geodash_main02.js
+++ 02_geometrydash/geodash_main02.js
@@ -39,8 +39,8 @@ function preload() {
   tileMap1 = loadStrings("stages/tiles1.txt");
 
   stereoMadness = loadSound("assests/stereo-madness.mp3")
-  deathSound = loadSound("assests/deathSound.mp3")
-  startSound = loadSound("assests/stereo-madness.mp3")
+  deathSound = loadSound("assests/death-sound.mp3")
+  startSound = loadSound("assests/game-start.mp3")
 
   
 }

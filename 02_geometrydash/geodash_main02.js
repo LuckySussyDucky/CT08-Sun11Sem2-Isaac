98de6ae22981d8d28f58a23dbd50d494c6a77096
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-26 03:29:32 +0000

    Jul 26, 2026, 3:29 AM

--- 02_geometrydash/geodash_main02.js
+++ 02_geometrydash/geodash_main02.js
@@ -117,7 +117,7 @@ function draw() {
       jumpChance -= 1;
     }
 
-    if (box.collides(ground)
+    if (box.collides(ground))
 
     if (box.collides(finishline)){
       gameoverLabel = new Sprite(); // x, y, width, height

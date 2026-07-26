19ee8c12ce90f9cd22c737524b246f72d78afecd
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-07-26 04:16:22 +0000

    Jul 26, 2026, 4:16 AM

--- 02_geometrydash/geodash_main02.js
+++ 02_geometrydash/geodash_main02.js
@@ -138,7 +138,7 @@ function draw() {
   }
 
   for (let orb of orbs){
-    if (box.colliding(orb)){
+    if (box.colliding(orb) && kb.){
       orb.visible = false;
       orb.collider = "none";
       box.vel.y = -10;

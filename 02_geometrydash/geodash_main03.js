d3e129ae90ee36802d01f28b7991272bfe3c0b12
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-02 03:21:14 +0000

    Aug 2, 2026, 3:21 AM

--- 02_geometrydash/geodash_main03.js
+++ 02_geometrydash/geodash_main03.js
@@ -106,7 +106,7 @@ function setup() {
   new Tiles(tileMap1, 0, 0, 50, 50);
 
   startSprite = new Sprite(width / 2, height / 2, 190, 90);
-  start
+  startSprite.img = startGame
 }
 
 function draw() {

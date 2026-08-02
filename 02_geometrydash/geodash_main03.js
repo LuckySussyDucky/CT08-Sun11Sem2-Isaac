cad3ce7dcba6515c0f12d4fa74da017a2d42ae52
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-02 04:03:29 +0000

    Aug 2, 2026, 4:03 AM

--- 02_geometrydash/geodash_main03.js
+++ 02_geometrydash/geodash_main03.js
@@ -46,7 +46,7 @@ function preload() {
 
   tileMap1 = loadStrings("stages/tiles1.txt");
   tileMap2 = loadStrings("stages/tiles2.txt");
-  tileMap3 = loadStrings("stages/tiles2.txt");
+  tileMap3 = loadStrings("stages/tiles3.txt");
 
 
   // stereoMadness = loadSound("assets/stereo-madness.mp3");

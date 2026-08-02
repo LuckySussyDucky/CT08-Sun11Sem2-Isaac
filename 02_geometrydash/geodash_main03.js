a548d36eeea4a49b3bdc643e3f6d411d4313dfe7
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-02 03:17:44 +0000

    Aug 2, 2026, 3:17 AM

--- 02_geometrydash/geodash_main03.js
+++ 02_geometrydash/geodash_main03.js
@@ -42,7 +42,7 @@ function preload() {
   background = loadImage("assets/geobg.png");
   spike = loadImage("assets/spike.png"); 
   startGame = loadImage("assets/clear.png");
-  endGame = loadImage("assets/startGame.png");
+  endGame = loadImage("assets/startgame.png");
 
   tileMap1 = loadStrings("stages/tiles1.txt");
 

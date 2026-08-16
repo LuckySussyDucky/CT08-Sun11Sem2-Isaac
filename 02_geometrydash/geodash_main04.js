9fb78ad4a3faa2641b0f47975de5989f6d6b0b00
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-16 03:44:42 +0000

    Aug 16, 2026, 3:44 AM

--- 02_geometrydash/geodash_main04.js
+++ 02_geometrydash/geodash_main04.js
@@ -62,7 +62,7 @@ function preload() {
 
   stereoMadness = createAudio("assets/stereo-madness.mp3");
   deathSound = createAudio("assets/geometry-dash-death-sound.mp3");
-  startSound = loadSound("assets/game-start.mp3");
+  startSound = createAudio("assets/game-start.mp3");
 
   
 }

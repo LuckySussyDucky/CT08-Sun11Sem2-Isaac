85d1990a6cbb4b1687a2b87bf6820f3578d86f8a
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-16 03:02:24 +0000

    Aug 16, 2026, 3:02 AM

---
+++ 02_geometrydash/geodash_main04.js
@@ -0,0 +1,259 @@
+//player box
+let box;
+
+// game variables
+let tileMap1;
+let tileMap2;
+let tileMap3;
+let startCoordinates;
+
+const MAX_JUMPS = 1;
+let jumpChance = MAX_JUMPS;
+
+let start = false;
+let gameOver = false;
+let endTimer = 0;
+
+let startSprite;
+let endSprite;
+
+let level = 1;
+let lastLevel;
+
+// world building groups
+let ground;
+let orb;
+let sharp;
+let finishline;
+
+// image sprites
+let cube;
+let background;
+let spike;
+let endGame;
+let startGame;
+
+// menu
+
+
+// sound assets
+let stereoMadness
+let deathSound
+let startSound
+
+
+function preload() {
+  cube = loadImage("assets/cube.png");
+  background = loadImage("assets/geobg.png");
+  spike = loadImage("assets/spike.png"); 
+  endGame = loadImage("assets/clear.png");
+  startGame = loadImage("assets/startgame.png");
+
+  tileMap1 = loadStrings("stages/tiles1.txt");
+  tileMap2 = loadStrings("stages/tiles2.txt");
+  tileMap3 = loadStrings("stages/tiles3.txt");
+
+
+  // stereoMadness = loadSound("assets/stereo-madness.mp3");
+  //deathSound = loadSound("assets/geometry-dash-death-sound.mp3");
+  //startSound = loadSound("assets/game-start.mp3");
+
+  
+}
+
+function setup() {
+  new Canvas(700, 600);
+  world.gravity.y = 32;
+  displayMode('centered');
+
+  box = new Sprite(50, height, 50, 50);
+  box.img = cube;
+  box.friction = 0;
+  box.bounciness = 0;
+  box.collider = "none";
+
+  startCoordinates = [50, (height - box.height / 2) - 25];
+  box.x = startCoordinates[0];
+  box.y = startCoordinates[1];
+
+  ground = new Group();
+  ground.tile = "g";
+
+  ground.w = 50;
+  ground.h = 50;
+  ground.collider = "static";
+  ground.color = "black";
+  ground.stroke = "rgba(0, 0, 0, 0)";
+
+  orbs = new Group();
+  orbs.tile = "o";
+
+  orbs.d = 24;
+  orbs.collider = "static";
+  orbs.color = "white";
+  orbs.strokeWeight = 0;
+
+  sharp = new Group();
+  sharp.tile = "s";
+
+  sharp.w = 25;
+  sharp.h = 25;
+  sharp.img = spike;
+  sharp.collider = "static";
+
+  finishline = new Group();
+  finishline.tile = "f";
+
+  finishline.w = 50;
+  finishline.h = 1200;
+  finishline.collider = "static";
+  finishline.visible = false;
+
+  particles = new Group();
+
+  new Tiles(tileMap1, 0, 0, 50, 50);
+
+  startSprite = new Sprite(width / 2, height / 2, 190, 90);
+  startSprite.img = startGame;
+  startSprite.collider = "none";
+}
+
+function draw() {
+  
+  clear()
+  image(background, 0, 0, 800, 600);
+
+
+  if (!start && (mouse.presses() || kb.presses("space"))){
+    start = true;
+    startSprite.visible = false;
+  } else if (!start){
+    if (frameCount % 60 < 30){
+      startSprite.visible = true;   
+    } else{
+      startSprite.visible = false;
+    }
+  }
+
+  if (start){
+    box.collider = "dynamic";
+    box.vel.x = 8;
+  
+    if (box.x >= width / 2){
+      camera.x = box. x;
+    } else{
+      camera.x = width / 2;
+    }
+
+    if (kb.presses("space") || mouse.presses("left") && jumpChance > 0){
+      box.vel.y = -10;
+      box.rotateTo(box.rotation + 90, 15);  
+      jumpChance -= 1;
+    }
+
+    // if (kb.presses("space") || mouse.presses("left")){
+    //   box.vel.y = -10;
+    //   box.rotateTo(box.rotation + 90, 15);
+    // }
+
+    if (box.collides(ground) && jumpChance < MAX_JUMPS){
+      jumpChance = MAX_JUMPS;
+    }
+
+    if (box.collides(sharp)){
+      resetGame();
+    }
+
+    for (let tile of ground){
+      if (box.colliding(tile)){
+        let leftEdge = tile.x - tile.w / 2;
+        let leftEdgeHeight = tile.y - tile.h / 2;
+
+        if (box.x < leftEdge && box.y > leftEdgeHeight){
+          resetGame();
+          break;
+        }
+      }
+    }
+
+    for (let orb of orbs){
+      if (box.colliding(orb)){
+        orb.visible = false;
+        orb.collider = "none";
+        box.vel.y = -10;
+        jumpChance = MAX_JUMPS;
+      }
+    }
+    
+    if (box.collides(finishline)){
+      finishGame();
+    }
+
+    if (gameOver){
+      if (frameCount - endTimer > 120){
+        if (endSprite){
+          endSprite.remove();
+        }
+
+        start = false;
+        gameOver = false;
+        resetGame();
+
+        level += 1;
+        loadLevel();
+      }
+    }
+  }
+} 
+
+function finishGame(){
+ if (!gameOver){
+    gameOver = true;
+    box.vel.x = 0;
+    jumpChance = 0;
+    endTimer = frameCount;
+
+    if (endSprite){
+      endSprite.remove()
+    }
+
+    endSprite = new Sprite(box.x, height / 2, 126, 24);
+    endSprite.collider = "none";
+    endSprite.img = endGame;
+  }
+}
+
+function resetGame(){
+  box.rotation = 0;
+
+  box.x = startCoordinates[0];
+  box.y = startCoordinates[1];
+
+  jumpChance = MAX_JUMPS;
+
+  camera.x = width / 2;
+
+  for (let orb of orbs){
+    orb.visible = true;
+    orb.collider = "static";
+  }
+}
+
+function loadLevel(){
+  ground.removeAll();
+  sharp.removeAll();
+  orbs.removeAll();
+  finishline.removeAll();
+
+  if (lastLevel < level){
+    level = 1;
+  }
+
+  if (level === 1){
+    new Tiles(tileMap1, 0, 0, 50, 50);
+  } else if (level === 2){
+    new Tiles(tileMap2, 0, 0, 50, 50);
+  } else if (level === 3){
+    new Tiles(tileMap3, 0, 0, 50, 50);
+  }
+}

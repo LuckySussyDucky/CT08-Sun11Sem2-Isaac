639b0cbbf9888ca3e1bfc5103e2b6e61696312a1
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-16 03:39:00 +0000

    Aug 16, 2026, 3:39 AM

--- 02_geometrydash/geodash_main04.js
+++ 02_geometrydash/geodash_main04.js
@@ -154,7 +154,7 @@ function draw() {
 
     if (kb.presses("space") || mouse.presses("left") && jumpChance > 0){
       box.vel.y = -10;
-      box.rotateTo(box.rotation + 360, 15);  
+      box.rotateTo(box.rotation + 359, 15);  
       jumpChance -= 1;
     }
 

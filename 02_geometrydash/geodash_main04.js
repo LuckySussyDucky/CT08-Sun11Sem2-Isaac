3ecae9c659338bf951a47f9918580e6b3755829e
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-16 03:43:45 +0000

    Aug 16, 2026, 3:43 AM

--- 02_geometrydash/geodash_main04.js
+++ 02_geometrydash/geodash_main04.js
@@ -212,8 +212,6 @@ function draw() {
     }
 
     if (frameCount % 3 === 0 && box.colliding(ground) && box.vel.x > 0.5){
-      box.rotation = 0;
-      
       let particle = new Sprite(box.x, box.y + box.h / 2, 8, 8, "none");
       particle.vel.x = -box.vel.x / 2;
       particle.vel.y = random(-2, 0);

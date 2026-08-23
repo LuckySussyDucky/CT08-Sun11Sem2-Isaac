7c7c0fe60e2cf30979eb6edfd0e572a351b3d16d
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-23 03:52:58 +0000

    Aug 23, 2026, 3:52 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -50,7 +50,9 @@ function draw() {
     image(video, 0, 0, videoW, videoH);
 
     for (let i = 0; i < hands.length; i++) {
-        let hand = hands[i]
+        let hand = hands[i];
+
+        for (let j = 0; j < hand.keypoints.length; j++)
     }
 }
 

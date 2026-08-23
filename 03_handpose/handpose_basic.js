77c90374c4321a5bbdf1dffb557c112a54f8e82a
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-23 03:22:47 +0000

    Aug 23, 2026, 3:22 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -11,7 +11,8 @@ function preload() {
         flipped: true,
         runtime: "tfjs",
         modelType: "full",
-        detectorModelUrl:
+        detectorModelUrl: undefined,
+        landmarkModelUrl: undefined,
     }
 }
 

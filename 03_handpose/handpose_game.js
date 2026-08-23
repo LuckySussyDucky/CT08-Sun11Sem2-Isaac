2b80da53481a12914838f3e50bad0f7371887d1c
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-23 04:17:33 +0000

    Aug 23, 2026, 4:17 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -60,7 +60,9 @@ function draw() {
         let hand = hands[0];
         let keypoint = hand.keypoints[8];
 
-        
+        fingerTip.x = keypoint.x
+        fingerTip.y = keypoint.y
+        fingerT
 
         circle(keypoint.x, keypoint.y, 30);
     }

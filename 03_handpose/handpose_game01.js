4a98f28ff037ae8e6b028a2a6b05b27b3cd8bf05
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-30 04:13:22 +0000

    Aug 30, 2026, 4:13 AM

--- 03_handpose/handpose_game01.js
+++ 03_handpose/handpose_game01.js
@@ -73,7 +73,7 @@ function setup() {
     rightWall = new Sprite(width, height / 2, 10, height, "static");
 
     boundaryGroup = new Group();
-    boundaryGroup.add(topWall)
+    boundaryGroup.add(topWall);
 }
 
 function draw() {

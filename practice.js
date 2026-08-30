791955e152ccd757a24a375a727227f9642a563b
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-30 03:43:32 +0000

    Aug 30, 2026, 3:43 AM

--- practice.js
+++ practice.js
@@ -13,8 +13,8 @@ function setup() {
     floor = new Sprite(400, 600, 1000000, 50);
     floor.collider = "static";
 
-    text("X", box.x, 20, 30);
-    text("y", box.x, 20, 30);
+    text("X: ", box.x, 20, 30);
+    text("Y: ", box.y, 20, 30);
 }
 
 function preload() {

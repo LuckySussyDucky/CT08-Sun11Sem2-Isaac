eac29868cc6f1d2d6d86554b3af0992d75f3752a
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-30 03:13:31 +0000

    Aug 30, 2026, 3:13 AM

--- practice.js
+++ practice.js
@@ -9,7 +9,8 @@ function setup() {
     world.gravity.y = 7
 
     box = new Sprite(600, 300, 30, 30);
-    box.x = 600
+    box.x = 600;
+    box.y = 300
     box.collider = "dynamic";
 
     floor = new Sprite(400, 600, 900, 50);

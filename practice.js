6362d94a4585631539c81b780de25bf3a6855f35
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-30 03:47:14 +0000

    Aug 30, 2026, 3:47 AM

--- practice.js
+++ practice.js
@@ -42,6 +42,6 @@ function draw() {
 
     if (mouse.presses("left")) {
         blocks = new Sprite(400, 300, 30, 30);
-        box.collider = "dynamic";
+        blocks.collider = "static";
     }
 }

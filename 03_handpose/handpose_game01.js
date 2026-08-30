21b1ea560574cff13f3c0969f32cb3f20919371b
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-30 04:11:27 +0000

    Aug 30, 2026, 4:11 AM

--- 03_handpose/handpose_game01.js
+++ 03_handpose/handpose_game01.js
@@ -68,7 +68,8 @@ function setup() {
     balloon.drag = 0.1;
 
     topWall = new Sprite(width / 2, 0, width, 10, "static");
-    bottomWall = new Sprite(width / 2, hright, width, 10, "static");
+    bottomWall = new Sprite(width / 2, height, width, 10, "static");
+    
 }
 
 function draw() {

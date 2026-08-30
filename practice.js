5ffb17bfb84ea8b16609ddd88f5860bc2573276d
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-08-30 03:26:00 +0000

    Aug 30, 2026, 3:26 AM

--- practice.js
+++ practice.js
@@ -25,7 +25,7 @@ function draw() {
         box.vel.y = -3;
     }
 
-    if (kb.holding("A") || kb.holding("")) {
+    if (kb.holding("A") || kb.holding("arrowLeft")) {
         box.x -= 3;
     }
 }

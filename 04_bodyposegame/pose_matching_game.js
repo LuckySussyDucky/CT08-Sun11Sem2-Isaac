034fc5e6a2628ed603cc8896e6cc065fe5648a86
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-09-20 04:20:32 +0000

    Sep 20, 2026, 4:20 AM

--- 04_bodyposegame/pose_matching_game.js
+++ 04_bodyposegame/pose_matching_game.js
@@ -136,7 +136,7 @@ function drawMiddleLine() {
     line(width / 2, 0, width / 2, cameraHeight);
 }
 
-function gotPoses(video, results) {
+function gotPoses(results) {
     detectedPeople = results;
 }
 

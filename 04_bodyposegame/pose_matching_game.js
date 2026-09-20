5b34524f64e2829af7d9856c6c51e672cf399624
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-09-20 04:17:09 +0000

    Sep 20, 2026, 4:17 AM

--- 04_bodyposegame/pose_matching_game.js
+++ 04_bodyposegame/pose_matching_game.js
@@ -85,11 +85,9 @@ function draw() {
     // Draw the middle line that separates Player 1 and Player 2 areas.
     drawMiddleLine();
     // Draw detection status.
-    
+    drawDetectionStatus();
 
     if (detectedPeople.length > 0) {
-
-        drawDetectionStatus();
         let pose = detectedPeople[0];
 
         let x = pose.nose.x + camera.x;

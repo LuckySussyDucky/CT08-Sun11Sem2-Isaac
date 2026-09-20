0c8478881ba7552c3eeb3429a1f1e8e6082ec536
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-09-20 04:08:04 +0000

    Sep 20, 2026, 4:08 AM

--- 04_bodyposegame/pose_matching_game.js
+++ 04_bodyposegame/pose_matching_game.js
@@ -90,7 +90,9 @@ function draw() {
     if (detectedPeople.length > 0) {
         let pose = detectedPeople[0];
 
-        let x = pose.nose.x
+        let x = pose.nose.x + camera.x;
+        let y = pose.nose.y;
+        
     }
 }
 

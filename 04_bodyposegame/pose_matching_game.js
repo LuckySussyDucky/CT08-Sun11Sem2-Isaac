853be329351d3db0f3213bab151b472d10017243
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2026-09-20 03:40:43 +0000

    Sep 20, 2026, 3:40 AM

--- 04_bodyposegame/pose_matching_game.js
+++ 04_bodyposegame/pose_matching_game.js
@@ -52,7 +52,8 @@ function setup() {
     let constraints = {
         video: {
             width: cameraWidth,
-            height: cameraHeight
+            height: cameraHeight,
+            aspectRatio: 
         }
     };
 }
